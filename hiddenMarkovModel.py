92c1bcb08664eaae406b71dd1c1c9df713b86550
neogeoss <neogeo-s@hanmail.net>
2020-02-17 02:07:59 +0900

    more comments to explain

--- hiddenMarkovModel.py
+++ hiddenMarkovModel.py
@@ -2,28 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+#It crates valid random markov matrix
 def random_normalized(d1, d2):
     x = np.random.random((d1, d2))
     return x/x.sum(axis=1, keepdims=True)
 
+
 class HMM:
     def __init__(self, M):
-        #number of hidden states
+        #this class gets number of hidden states in constructor
         self.M = M
 
+        #iteration limit is set by max_iter variable
     def fit(self, X, max_iter=30):
+
         t0 = datetime.now()
         np.random.seed(123)
+        #
         # train the HMM model using the Baum-Welch algorithm
         # a specific instance of the expectation-maximization algorithm
 
         # determine V, the vocabulary size
-        # assume observables are already integers from 0..V-1
+        # assume observables are already integers from 0 to 1
         # X is a jagged array of observed sequences
         V = max(max(x) for x in X) + 1
         N = len(X)
 
-        self.pi = np.ones(self.M) / self.M # initial state distribution
+        self.pi = np.ones(self.M) / self.M # initial state which is uniform distribution
         self.A = random_normalized(self.M, self.M) # state transition matrix
         self.B = random_normalized(self.M, V) # output distribution
 
@@ -32,37 +37,46 @@ class HMM:
 
         costs = []
         for it in range(max_iter):
+            # prints every 10 cycle
             if it % 10 == 0:
                 print("it:", it)
+
+            #
             alphas = []
             betas = []
             P = np.zeros(N)
+            #loop through each observation
             for n in range(N):
+                # x is nth observation
                 x = X[n]
+                # length of each little x
                 T = len(x)
+
                 alpha = np.zeros((T, self.M))
+                # 1st value of alpha is pi * 1st observation
                 alpha[0] = self.pi*self.B[:,x[0]]
+                #
                 for t in range(1, T):
+                    #
                     tmp1 = alpha[t-1].dot(self.A) * self.B[:, x[t]]
-                    # tmp2 = np.zeros(self.M)
-                    # for i in range(self.M):
-                    #     for j in range(self.M):
-                    #         tmp2[j] += alpha[t-1,i] * self.A[i,j] * self.B[j, x[t]]
-                    # print "diff:", np.abs(tmp1 - tmp2).sum()
+
                     alpha[t] = tmp1
                 P[n] = alpha[-1].sum()
                 alphas.append(alpha)
 
+                #beta is also T by M matrix
+                #beta counts backwards
                 beta = np.zeros((T, self.M))
                 beta[-1] = 1
                 for t in range(T - 2, -1, -1):
                     beta[t] = self.A.dot(self.B[:, x[t+1]] * beta[t+1])
                 betas.append(beta)
 
-            # print "P:", P
-            # break
+
             assert(np.all(P > 0))
+            # calculate the total log likelihood
             cost = np.sum(np.log(P))
+            # appended to list of cost
             costs.append(cost)
 
             # now re-estimate pi, A, B
@@ -70,28 +84,23 @@ class HMM:
             # print "self.pi:", self.pi
             # break
 
+            # keep track of all denominators and numerators for A and B updates
             den1 = np.zeros((self.M, 1))
             den2 = np.zeros((self.M, 1))
             a_num = 0
             b_num = 0
+            #
             for n in range(N):
                 x = X[n]
                 T = len(x)
-                # print "den shape:", den.shape
-                # test = (alphas[n][:-1] * betas[n][:-1]).sum(axis=0, keepdims=True).T
-                # print "shape (alphas[n][:-1] * betas[n][:-1]).sum(axis=0): ", test.shape
+                # accumulate everything except the last one
                 den1 += (alphas[n][:-1] * betas[n][:-1]).sum(axis=0, keepdims=True).T / P[n]
+                #sum of all things
                 den2 += (alphas[n] * betas[n]).sum(axis=0, keepdims=True).T / P[n]
 
-                # tmp2 = np.zeros((self.M, 1))
-                # for i in range(self.M):
-                #     for t in range(T-1):
-                #         tmp2[i] += alphas[n][t,i] * betas[n][t,i]
-                # tmp2 /= P[n]
-                # # print "diff:", np.abs(tmp1 - tmp2).sum()
-                # den += tmp1
 
-                # numerator for A
+
+                # updating numerator for A
                 a_num_n = np.zeros((self.M, self.M))
                 for i in range(self.M):
                     for j in range(self.M):
@@ -99,26 +108,13 @@ class HMM:
                             a_num_n[i,j] += alphas[n][t,i] * self.A[i,j] * self.B[j, x[t+1]] * betas[n][t+1,j]
                 a_num += a_num_n / P[n]
 
-                # numerator for B
-                # b_num_n = np.zeros((self.M, V))
-                # for i in range(self.M):
-                #     for j in range(V):
-                #         for t in range(T):
-                #             if x[t] == j:
-                #                 b_num_n[i,j] += alphas[n][t][i] * betas[n][t][i]
+
                 b_num_n2 = np.zeros((self.M, V))
                 for i in range(self.M):
                     for t in range(T):
                         b_num_n2[i,x[t]] += alphas[n][t,i] * betas[n][t,i]
                 b_num += b_num_n2 / P[n]
-            # tmp1 = a_num / den1
-            # tmp2 = np.zeros(a_num.shape)
-            # for i in range(self.M):
-            #     for j in range(self.M):
-            #         tmp2[i,j] = a_num[i,j] / den1[i]
-            # print "diff:", np.abs(tmp1 - tmp2).sum()
-            # print "tmp1:", tmp1
-            # print "tmp2:", tmp2
+
             self.A = a_num / den1
             self.B = b_num / den2
             # print "P:", P
@@ -146,9 +142,11 @@ class HMM:
     def likelihood_multi(self, X):
             return np.array([self.likelihood(x) for x in X])
 
+    #log print
     def log_likelihood_multi(self, X):
             return np.log(self.likelihood_multi(X))
 
+    #viterbi algorithms
     def get_state_sequence(self, x):
             T = len(x)
             delta = np.zeros((T, self.M))
